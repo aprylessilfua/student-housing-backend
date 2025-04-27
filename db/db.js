1a6dc6d0711d578c0a0af4fcecb0a46bbe5b728b
aprylessilfua <aprylpoku@gmail.com>
2025-04-27 21:20:46 +0000

    Update .gitignore to ignore .env file

--- db/db.js
+++ db/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
 
 module.exports = pool;
 
-//----
+//-
 
 
 //const { Pool } = require('pg');
--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 const db = require('./db');
 
-// Example script: Fetch all students
 (async () => {
   console.log('Fetching all students...');
   const result = await db.query('SELECT * FROM users WHERE role = $1', ['student']);
