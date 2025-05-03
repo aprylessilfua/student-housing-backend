c89de0d73306eef2aec3e452e7412a34c6562af8
aprylessilfua <aprylpoku@gmail.com>
2025-05-03 19:24:49 +0000

    Update applications.js

--- routes/applications.js
+++ routes/applications.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const { getApplications, createApplication } = require('../controllers/applicationsController');
+const {
+  getApplications,
+  createApplication,
+  updateApplication
+} = require('../controllers/applicationsController');
+
 const router = express.Router();
 
-//Routes for applications
+// Fetch all applications
 router.get('/', getApplications);
+
+// Create a new application
 router.post('/', createApplication);
 
+// Update application status (approve/reject)
+router.put('/:id', updateApplication);
+
 module.exports = router;
+
